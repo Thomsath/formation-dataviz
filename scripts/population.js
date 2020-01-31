84af26c120dfeb25732ecdd036a9bbfcd18527c6
Thomas <thomas.beaupertuis@hotmail.fr>
2020-01-31 11:32:46 +0100

    Update population.js

--- scripts/population.js
+++ scripts/population.js
@@ -12,7 +12,6 @@ function generateItem(elt) {
         [0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f'][Math.floor(Math.random()*16)])
         && (lor.length == 6) ?  lor : co(lor); })('');
     item.style.height = elt.population/10000 + "px";
-    item.style.width = '50px';
     item.classList.add = 'stick';
     return item;
   }
